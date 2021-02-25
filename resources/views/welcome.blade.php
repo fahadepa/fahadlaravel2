ce716ba74a9e866ad65ab1df42a382ae7239d385
fahad ahmad <fahadepa@gmail.com>
2021-02-25 22:26:21 +0500

    new welcome commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Fahad Get Ready to Start Laravel </title>
+        <title>Fahad Get Ready to Start </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
